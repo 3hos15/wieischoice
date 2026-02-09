e27fc5db2aa94a4f0836883a03585d63cd994237
3hos15 <145386421+3hos15@users.noreply.github.com>
2026-02-09 20:49:27 +0100

    9 feb

--- script/script.js
+++ script/script.js
@@ -35,4 +35,57 @@ headArea.addEventListener("drop", function (event) {
 });
 
 
+const tabs = document.querySelectorAll(".tabs .tab");
+const sections = document.querySelectorAll("main > section");
 
+function showSection(className) {
+  sections.forEach(section => {
+    section.classList.toggle("active", section.classList.contains(className));
+  });
+
+  tabs.forEach(tab => {
+    tab.classList.toggle("active", tab.dataset.tab === className);
+  });
+}
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    showSection(tab.dataset.tab);
+  });
+});
+
+
+showSection("travelPics");
+
+
+
+// const base = "https://fdnd.directus.app/items"
+// let endpoint = "/person?filter[squads][squad_id][tribe][name]=CMD%20Minor%20Web%20Dev&filter[squads][squad_id][cohort]=2526&sort=name&sort=name&limit=5"
+// let url = base + endpoint
+
+// let deLijst = document.querySelector("ul")
+
+// getMinorMensen()
+
+// async function getMinorMensen() {
+//     let response = await fetch(url)
+
+//     let responseJSON =  await response.json();
+
+//     let deMinorMensen = responseJSON.data
+
+//     console.log(deMinorMensen)
+
+//     deMinorMensen.forEach( eenMinorMens =>{
+//         console.log(eenMinorMens)
+
+//         let minorMensHTML = 
+//             `<li>
+//                 <h2>${eenMinorMens.name}</h2>
+//                 <p>${eenMinorMens.fav_tag}</p>
+//             </li>`
+
+//             deLijst.insertAdjacentHTML("beforeend", minorMensHTML)
+//             // afterbegin kan ook
+//     } )
+// }
